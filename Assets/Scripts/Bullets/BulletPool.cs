c229720a49a54042ad2bc58f0d8ff68ba2cdeb2d
Ashwin02 <ashwinabraham2001@gmail.com>
2025-06-02 16:06:00 +0530

    Created GetBullet() method inside BulletPPool

--- Assets/Scripts/Bullets/BulletPool.cs
+++ Assets/Scripts/Bullets/BulletPool.cs
@@ -15,6 +15,31 @@ namespace CosmicCuration.Bullets
             this.bulletScriptableObject = bulletScriptableObject;
         }
 
+        public BulletController GetBullet()
+        {
+            if(pooledBullets.Count > 0)
+            {
+                PooledBullet pooledBullet = pooledBullets.Find(item => item.isUsed==false);
+                if(pooledBullet != null)
+                {
+                    //means we have found a bullet no being used
+                    pooledBullet.isUsed = true;
+                    return pooledBullet.Bullet;
+                }
+            }
+            return CreateNewPooledBullet();
+        }
+
+        private BulletController CreateNewPooledBullet()
+        {
+            PooledBullet pooledBullet = new PooledBullet();
+            pooledBullet.Bullet = new BulletController(bulletView,bulletScriptableObject);
+            pooledBullet.isUsed=true;
+            pooledBullets.Add(pooledBullet);
+
+            return pooledBullet.Bullet;
+        }
+
         public class PooledBullet
         {
             public bool isUsed;
