df4b003f64ae23dab3bc95bf3c5439925cdefd22
Ashwin02 <ashwinabraham2001@gmail.com>
2025-06-03 15:21:57 +0530

    Added Generic Object Pool script and implemented GetItem() method

---
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+namespace CosmicCuration.Utilities
+{
+    public class GenericObjectPool<T> where T : class
+    {
+        private List<PooledItem<T>> pooledItems = new List<PooledItem<T>>();
+
+        protected T GetItem()
+        {
+            if(pooledItems.Count > 0)
+            {
+                PooledItem<T> item = pooledItems.Find(item => item.isUsed == false);
+
+                if(item != null)
+                {
+                    item.isUsed = true;
+                    return item.Item;
+                }
+            }
+            return CreateNewPooledItem();
+        }
+
+        private T CreateNewPooledItem()
+        {
+            PooledItem<T> newItem = new PooledItem<T>();
+            newItem.Item = CreateNewItem();
+            newItem.isUsed = true;
+            pooledItems.Add(newItem);
+
+            return newItem.Item;
+        }
+
+        protected virtual T CreateNewItem()
+        {
+            throw new NotImplementedException("Child class did not implement CreateNewItem()");
+        }
+
+        public class PooledItem<T>
+        {
+            public T Item;
+            public bool isUsed;
+        }
+    }
+}
